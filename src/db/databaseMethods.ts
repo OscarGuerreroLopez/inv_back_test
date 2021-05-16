95080cb7d5c80da835fb9d537f3986d49d28f276
Oscar Guerrero Lopez <oscar.computer.guy@gmail.com>
2021-05-16 16:52:11 +0200

    fix: cleaning console logs

--- src/db/databaseMethods.ts
+++ src/db/databaseMethods.ts
@@ -11,11 +11,6 @@ export const DatabaseMethods = (
   ) => Promise<T>;
 }> => {
   const remove = async (collection: string, where: IObjectLiteral) => {
-    console.log(
-      "@@@remove",
-      await database.collection(collection).delete(where),
-    );
-
     return await database.collection(collection).delete(where);
   };
 
@@ -23,8 +18,6 @@ export const DatabaseMethods = (
     collection: string,
     where: IObjectLiteral = {},
   ): Promise<T> => {
-    console.log("@@@find", await database.collection(collection).find(where));
-
     return await database.collection(collection).find(where);
   };
 
@@ -32,11 +25,6 @@ export const DatabaseMethods = (
     collection: string,
     where: IObjectLiteral,
   ): Promise<T> => {
-    console.log(
-      "@@@findOne",
-      await database.collection(collection).findOne(where),
-    );
-
     return await database.collection(collection).findOne(where);
   };
 
@@ -48,7 +36,6 @@ export const DatabaseMethods = (
     const updatedRecord = await database
       .collection(collection)
       .updateOne(where, values);
-    console.log("@@@updateOne", updatedRecord);
 
     return updatedRecord;
   };
